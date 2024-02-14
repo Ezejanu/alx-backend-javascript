5836437ed1ec4fc1deb656646fbc362967477f4c
Ezejanu <Ezejanu_nno@ymail.com>
2024-02-14 02:29:40 +0000

    task 9 and 10

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+export default function updateUniqueItems(initialMap) {
+  if (!(initialMap instanceof Map)) {
+	  throw Error('Cannot process');
+  }
+  const updatedMap = new Map();
+
+  for (const [key, value] of initialMap.entries()) {
+      updatedMap.set(key, value === 1 ? 100 : value);
+    }
+  return updatedMap;
+}
